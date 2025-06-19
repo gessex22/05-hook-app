85f6b9b2ec99d1577c2ceff0ac426d284809f373
gessex22 <gessembergsteiner@gmail.com>
2025-06-19 03:26:51 -0500

    warnings on hooks/ unmount

--- src/02-useEffect.jsx/Message.jsx
+++ src/02-useEffect.jsx/Message.jsx
@@ -1,24 +1,28 @@
-import React, {  useEffect } from 'react'
+import React, { useEffect } from "react";
 
 export const Message = () => {
+  useEffect(() => {
+      
+        const onMouseMove = ( {x, y})=>{
+        
+          const coords =  {x, y} 
+          console.log(coords)
 
- useEffect(() => {
-  console.log('mensaje montado')
- 
-   return () => {
-        console.log('mensaje unmounted')
-   }
- }, [])
- 
+          }
 
+          window.addEventListener('mousemove', onMouseMove)
+          return()=>{
+            window.removeEventListener('mousemove',onMouseMove)
+          
+          }
+        
+        }
+      
+      , []);
 
   return (
     <>
-        <h3>
-            Usuario ya existe
-        </h3>
-    
-
+      <h3>Usuario ya existe</h3>
     </>
-  )
-}
+  );
+};
