71821fccddcb49c895091a591399844e029e9b73
gessex22 <gessembergsteiner@gmail.com>
2025-07-24 00:13:39 -0500

    Navbar NavLink

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,28 @@
+import { Navigate, Route, Routes, Link } from "react-router";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+import { Navbar } from "./Component/Navbar";
+
+export const MainApp = () => {
+  return (
+    <>
+      <h1> Main App</h1>
+        {/* <a href="/">Home</a>
+        <a href="/about"> About</a>
+        <a href="/login">Login</a> */}
+
+        <Navbar/>
+      
+              <hr />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+        {/* <Route path="/*" element={<LoginPage/>} /> */}
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHook } from './03-examples/MultipleCustomhook'
-import { FoscusScreen } from './04-useRef/FoscusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHooks'
-import { CallBackHook } from './06-memos/CallBackHook'
-import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHook } from "./03-examples/MultipleCustomhook";
+import { FoscusScreen } from "./04-useRef/FoscusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
+import { Memorize } from "./06-memos/Memorize";
+import { MemoHook } from "./06-memos/MemoHooks";
+import { CallBackHook } from "./06-memos/CallBackHook";
+import { Padre } from "./07-tarea-memo/07-tarea-memo/Padre";
+import { MainApp } from "./09-useContext/mainApp";
+import { BrowserRouter } from "react-router";
 // import { HookApp } from './HooksApp'
 
-import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <TodoApp/>
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
   </StrictMode>
-)
+);
