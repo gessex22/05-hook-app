36739e931ad4d0722f18343e2790d565541ed492
gessex22 <gessembergsteiner@gmail.com>
2025-08-01 19:51:33 -0500

    test useCounter

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,29 +1,61 @@
 import { render, renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
+import { act } from "react";
 
 describe("Test on useCounter", () => {
-  test("Should return default values" ,
-    () => {
-      const { result } = renderHook(() => useCounter());
-      const { counter, decrease, increment, reset } = result.current
+  test("Should return default values", () => {
+    const { result } = renderHook(() => useCounter());
+    const { counter, decrease, increment, reset } = result.current;
 
-      expect(counter).toBe(10);
-      expect( decrease).toEqual( expect.any(Function))
-      expect( increment).toEqual( expect.any(Function))
-      expect( reset).toEqual( expect.any(Function))
-    
-    
-    })
+    expect(counter).toBe(10);
+    expect(decrease).toEqual(expect.any(Function));
+    expect(increment).toEqual(expect.any(Function));
+    expect(reset).toEqual(expect.any(Function));
+  });
+
+  test("Should to has the counter with the value 100", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { counter, decrease, increment, reset } = result.current;
+
+    expect(counter).toBe(100);
+  });
+
+  test("should to increase counter ", () => {
+    const { result } = renderHook(() => useCounter());
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+      increment(2)
+    });
+
+    expect(result.current.counter).toBe(13 );
+  });
+
+   test("should to DECREASE THE counter ", () => {
+    const { result } = renderHook(() => useCounter());
+    const { decrease } = result.current;
+
+    act(() => {
+      decrease();
+      decrease(2)
+    });
+
+    expect(result.current.counter).toBe(7 );
+  });
+
+   test("should to reset THE counter ", () => {
+    const { result } = renderHook(() => useCounter());
+    const { reset,increment, counter } = result.current;
+
+    act(() => {
+    increment(20)
+    reset()
+    });
+
+    expect(result.current.counter).toBe(counter);
+  });
 
 
-    test("Should to has the counter with the value 100" ,
-    () => {
-      const { result } = renderHook(() => useCounter(100));
-      const { counter, decrease, increment, reset } = result.current
 
-      expect(counter).toBe(100);
-     
-    
-    
-    })
 });
---
+++ tests/hooks/useForm.test.js
@@ -0,0 +1,56 @@
+import { renderHook } from "@testing-library/react";
+import { useForm } from "../../src/hooks/useForm";
+import { act } from "react";
+
+    const initialForm = {
+      name: "eliot Anderson",
+      mail: "eliotanderson2025@gmail.com",
+    };
+describe("Test on useForm", () => {
+  test("Should to return default values", () => {
+    const { result } = renderHook(() => useForm(initialForm));
+
+    expect( result.current).toEqual( {
+        name: initialForm.name,
+        mail: initialForm.mail,
+        formState: initialForm,
+        onImputChange: expect.any(Function),
+        onResetForm: expect.any(Function)
+    
+    
+    })
+  });
+
+
+  test("should to change the value of name", ()=>{
+  
+  
+    const { result} = renderHook( ()=> useForm(initialForm) )
+    const { onImputChange,formState } = result.current
+    
+        act( ()=>{
+        onImputChange({target: { name: "name",  value: "Anderson Eliot"}})
+    
+    })
+    expect( result.current.formState.name).toEqual( "Anderson Eliot")
+
+  
+  })
+
+      test("should to reset the form", ()=>{
+  
+  
+    const { result} = renderHook( ()=> useForm(initialForm) )
+    const { onImputChange,onResetForm,formState } = result.current
+    
+        act( ()=>{
+        onImputChange({target: { name: "name",  value: "Anderson Eliot"}})
+        onResetForm()
+    
+    })
+    expect( result.current.formState).toEqual( initialForm)
+
+  
+  })
+
+});
